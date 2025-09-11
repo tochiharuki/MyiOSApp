d6f8900f1f3b3079f5d1c83e8edb6bc26dbe1879
Tochishita Haruki <tochishitaharuki@Haru74-2.local>
2025-09-12 03:51:26 +0900

    Initial commit for forexgame project

---
+++ MyiOSApp/MyiOSApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyiOSApp
+//
+//  Created by Tochishita Haruki on 2025/09/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyiOSApp/MyiOSApp/MyiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyiOSAppApp.swift
+//  MyiOSApp
+//
+//  Created by Tochishita Haruki on 2025/09/12.
+//
+
+import SwiftUI
+
+@main
+struct MyiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
