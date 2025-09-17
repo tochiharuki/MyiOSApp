f87407b4fa32f8588bd1bf16bf685e2180fb79a5
Tochishita Haruki <tochishitaharuki@Haru74-2.local>
2025-09-17 12:14:43 +0900

    Add iOS build workflow

--- MyiOSApp/MyiOSApp/ContentView.swift
+++ MyiOSApp/MyiOSApp/ContentView.swift
@@ -1,21 +1,19 @@
-//
-//  ContentView.swift
-//  MyiOSApp
-//
-//  Created by Tochishita Haruki on 2025/09/12.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+        ZStack {
+            // 背景色を薄い水色に設定
+            Color(red: 0.85, green: 0.95, blue: 1.0)
+                .edgesIgnoringSafeArea(.all)
+            
+            // 文字を中央に配置
+            Text("領収書くん")
+                .font(.system(size: 48, weight: .bold)) // 大きめの太字
+                .multilineTextAlignment(.center)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .padding()
         }
-        .padding()
     }
 }
 
