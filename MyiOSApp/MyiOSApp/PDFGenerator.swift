30dd7e292d0cbe6aa5bb4817adcc30dcd2df14a6
Tochishita Haruki <tochishitaharuki@Haru74-2.local>
2025-09-24 23:54:36 +0900

    all

--- MyiOSApp/MyiOSApp/PDFGenerator.swift
+++ MyiOSApp/MyiOSApp/PDFGenerator.swift
@@ -5,10 +5,10 @@ import UIKit
 // 共通フォント管理
 private enum ReceiptFont {
     static func regular(size: CGFloat) -> UIFont {
-        return UIFont(name: "RyuminL-KL", size: size) ?? ReceiptFont.regular(size: size)
+        return UIFont(name: "HiraMinProN-W3", size: size) ?? UIFont.systemFont(ofSize: size)
     }
     static func bold(size: CGFloat) -> UIFont {
-        return UIFont(name: "RyuminB-KL", size: size) ?? ReceiptFont.bold(size: size)
+        return UIFont(name: "HiraMinProN-W6", size: size) ?? UIFont.boldSystemFont(ofSize: size)
     }
 }
 
