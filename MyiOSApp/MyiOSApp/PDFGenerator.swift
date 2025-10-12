dccede4354a40db146a888ef1ff1ab7dff9fb459
Tochishita Haruki <tochishitaharuki@Haru74-2.local>
2025-10-12 21:09:54 +0900

    all

--- MyiOSApp/MyiOSApp/PDFGenerator.swift
+++ MyiOSApp/MyiOSApp/PDFGenerator.swift
@@ -120,7 +120,7 @@ struct PDFGenerator {
             
             // --- 右側の起点（但し書き・内訳を右にまとめる） ---
             let contentTop: CGFloat = amountY + amountSize.height + 24
-            let rightAreaX: CGFloat = pageWidth / 2 - 20
+            let rightAreaX: CGFloat = pageWidth / 2 - -80
             let rightAreaWidth: CGFloat = pageWidth - rightAreaX - 80
             
             // --- 但し書き（右側） ---
@@ -162,8 +162,6 @@ struct PDFGenerator {
                 let stampWidth: CGFloat = 100
                 let stampHeight: CGFloat = 100
                 let stampX: CGFloat = 80
-                // stamp を右側要素と重ならない高さにする：contentTop を基準にする
-                var stampY: CGFloat = contentTop
                 // 収入印紙がページ下にはみ出すのを防ぐため調整
                 let bottomMargin: CGFloat = 30
                 if stampY + stampHeight + bottomMargin > pageHeight {
